acde7ccf7871676c4810fdce49a9619785ff0268
gechunxi_pro14 <1193700248@qq.com>
2024-10-29 17:10:22 +0800

    部分文件添加UIKIt

--- Transition/Classes/Helpers/TransitionEdges.swift
+++ Transition/Classes/Helpers/TransitionEdges.swift
@@ -21,7 +21,7 @@
 //    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //    SOFTWARE.
 
-import CoreFoundation
+import UIKit
 
 public enum TransitionScreenEdge {
     case top, right, bottom, left
